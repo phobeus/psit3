2d922ac34f09471bceedb8a79f56bddb84fd19be
Pascal Stählin <pascal.staehlin@gmail.com>
2014-11-23 09:56:29 +0100

    Removed unused files

--- app/src/main/java/info/movito/themoviedbapi/TvResults.java
+++
@@ -1,9 +0,0 @@
-package info.movito.themoviedbapi;
-
-import info.movito.themoviedbapi.model.core.ResultsPage;
-import info.movito.themoviedbapi.model.tv.TvSeries;
-
-
-public class TvResults extends ResultsPage<TvSeries> {
-
-}
--- app/src/main/java/info/movito/themoviedbapi/model/core/MovieResults.java
+++
@@ -1,8 +0,0 @@
-package info.movito.themoviedbapi.model.core;
-
-import info.movito.themoviedbapi.model.MovieDb;
-
-
-public class MovieResults extends ResultsPage<MovieDb> {
-
-}

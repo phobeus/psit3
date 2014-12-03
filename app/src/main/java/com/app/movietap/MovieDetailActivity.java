1b8dab6149e3cd4c39f38cb303540e8070d7bc4c
Andreas Plüss <andreaspluess@gmail.com>
2014-12-03 20:03:58 +0100

    MovieDetail view optimized still small bug by clicking buttons ant top right

--- app/src/main/java/com/app/movietap/MovieDetailActivity.java
+++ app/src/main/java/com/app/movietap/MovieDetailActivity.java
@@ -1,5 +1,6 @@
 package com.app.movietap;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,6 +29,12 @@ public class MovieDetailActivity extends BaseActivity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_movie_detail);
 
+    //set drawable icons
+    final Drawable drawableRemember = getResources().getDrawable(R.drawable.ic_library);
+    final Drawable drawableWish = getResources().getDrawable(R.drawable.ic_gift);
+    final Drawable drawableRememberAdded = getResources().getDrawable(R.drawable.ic_library_added);
+    final Drawable drawableWishAdded = getResources().getDrawable(R.drawable.ic_gift_added);
+
     //get information from last activity
     _movie = (Movie) getIntent().getExtras().getParcelable("movie");
 
@@ -37,37 +44,65 @@ public class MovieDetailActivity extends BaseActivity
 
     //save image from the movie locally and display it
     _aQuery = new AQuery(this);
-    _aQuery.id(R.id.movieDetail_imageViewPoster).image("http://image.tmdb.org/t/p/w300" + _movie.getPoster());
+    _aQuery.id(R.id.movieDetail_imageViewPoster).image("http://image.tmdb.org/t/p/w500" + _movie.getPoster());
+      /*sizes are 500 or 780*/
 
+      
+    //check if movie exists in Wishlist
     PersistenceHandler handler = new PersistenceHandler(this);
     Object result = handler.loadWhere(StoredMovie.class, "MovieId = ?", new String[]{_movie.getId() + ""}, null, null, null);
-    if (result != null)
-    {
-      _storedMovie = (StoredMovie) result;
-      ((Button) findViewById(R.id.movieDetail_buttonWish)).setText("von Wunschliste entfernen");
+    if (result != null) {
+        _storedMovie = (StoredMovie) result;
+        //((Button) findViewById(R.id.movieDetail_buttonWish)).setText("von Wunschliste entfernen");
+        ((Button) findViewById(R.id.movieDetail_buttonWish)).setCompoundDrawablesWithIntrinsicBounds(drawableWishAdded, null, null, null);
+        //change Image, to see that move is stored
     }
+      
+    //check if movie exists in remember list
+    /*
+     *
+     *like code above, but regarding the set enum blablabl
+     */
 
-    Button favoritesButton = (Button) findViewById(R.id.movieDetail_buttonWish);
-    favoritesButton.setOnClickListener(new View.OnClickListener()
-    {
-      @Override
-      public void onClick(View view)
-      {
-        PersistenceHandler handler = new PersistenceHandler(MovieDetailActivity.this);
-        if (_storedMovie != null)
-        {
-          handler.deleteWhere(StoredMovie.class, "Id = ?", new String[]{_storedMovie.Id + ""});
-          ((Button) findViewById(R.id.movieDetail_buttonWish)).setText("zu Wunschliste hinzufügen");
-        } else
-        {
-          _storedMovie = new StoredMovie(_movie);
-          _storedMovie.Status = MovieStatus.WantToSee;
-          _storedMovie.Shared = MovieSharedStatus.WithNobody;
-          _storedMovie.Id = handler.save(_storedMovie);
-          ((Button) findViewById(R.id.movieDetail_buttonWish)).setText("von Wunschliste entfernen");
-        }
-      }
-    });
+      Button wishButton = (Button) findViewById(R.id.movieDetail_buttonWish);
+      wishButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              PersistenceHandler handler = new PersistenceHandler(MovieDetailActivity.this);
+              if (_storedMovie != null) {
+                  handler.deleteWhere(StoredMovie.class, "Id = ?", new String[]{_storedMovie.Id + ""});
+                  //((Button) findViewById(R.id.movieDetail_buttonWish)).setText("zu Wunschliste hinzufügen");
+                  ((Button) findViewById(R.id.movieDetail_buttonWish)).setCompoundDrawablesWithIntrinsicBounds(drawableWish, null, null, null);
+              } else {
+                  _storedMovie = new StoredMovie(_movie);
+                  _storedMovie.Status = MovieStatus.WantToSee;
+                  _storedMovie.Shared = MovieSharedStatus.WithNobody;
+                  _storedMovie.Id = handler.save(_storedMovie);
+                  //((Button) findViewById(R.id.movieDetail_buttonWish)).setText("von Wunschliste entfernen");
+                  ((Button) findViewById(R.id.movieDetail_buttonWish)).setCompoundDrawablesWithIntrinsicBounds(drawableWishAdded, null, null, null);
+              }
+          }
+      });
+      
+      Button rememberButton = (Button) findViewById(R.id.movieDetail_buttonRemember);
+      rememberButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              PersistenceHandler handler = new PersistenceHandler(MovieDetailActivity.this);
+              if (_storedMovie != null) {
+                  handler.deleteWhere(StoredMovie.class, "Id = ?", new String[]{_storedMovie.Id + ""});
+                  //((Button) findViewById(R.id.movieDetail_buttonWish)).setText("zu Merkliste hinzufügen");
+                  ((Button) findViewById(R.id.movieDetail_buttonRemember)).setCompoundDrawablesWithIntrinsicBounds(drawableRemember, null, null, null);
+              } else {
+                  _storedMovie = new StoredMovie(_movie);
+                  _storedMovie.Status = MovieStatus.WantToSee;
+                  _storedMovie.Shared = MovieSharedStatus.WithNobody;
+                  _storedMovie.Id = handler.save(_storedMovie);
+                  //((Button) findViewById(R.id.movieDetail_buttonWish)).setText("von Merkliste entfernen");
+                  ((Button) findViewById(R.id.movieDetail_buttonRemember)).setCompoundDrawablesWithIntrinsicBounds(drawableRememberAdded, null, null, null);
+              }
+          }
+      });
 
     //moreinfo of the same movie
     Movie detailedMovie = ApiTools.getMovie(_movie.getId());
